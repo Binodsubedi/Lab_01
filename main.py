2c8c294fc0f04a82270ccfbddd1c3b0addd55d66
dell <binodsubedi125@gmail.com>
2021-06-10 14:35:46 +0545

    Time finding

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+import math
+
 P_value = float(input('Enter the number of minutes:'))
 P_hour = P_value/60
 point = ''
+wp = math.floor(P_hour)
+
+if P_hour > wp:
+    minutes = (P_hour - wp) * 60
+    P_hour = wp
+    minu = math.floor(minutes)
+    int(P_hour)
+
 
 if P_hour<=12:
     point = 'am'
@@ -12,6 +22,6 @@ if P_hour> 12:
 
     P_hour -= 12
 
-print(f'the time now is {P_hour} {point}')
+print(f'the time now is {P_hour}:{minu} {point}')
 
 
